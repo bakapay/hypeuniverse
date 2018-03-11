944447bba7bd219ab50c1be8cf5958e7f78948ef
zapayyyy <36130036+zapayyyy@users.noreply.github.com>
2018-03-11 15:18:28 +0200

    Update ship.js

--- commands/ship.js
+++ commands/ship.js
@@ -3,7 +3,7 @@ exports.run = function(client, message, args) {
 let xemoji = client.guilds.get("418461344707248129").emojis.find("name", "CHX")
 let vemoji = client.guilds.get("418461344707248129").emojis.find("name", "CHV")
     let user = message.mentions.users.first();
-	const rating = Math.floor((Math.random() * 100));
+	const rating = Math.floor((Math.random() * 120));
 const k = new Discord.RichEmbed()
 .setAuthor("Command: /ship", client.user.avatarURL)
 .setDescription("**Description:** Check your relationship with another person\n**Usage:** /ship [user]\n**Example:** /ship <@419132332407128064>")
@@ -15,7 +15,7 @@ if(user.id === message.author.id) return message.channel.send("**" + message.aut
 if(rating < 25) return message.channel.send("**" + message.author.username + "** :heart: **" + user.username + "** **|** **" + rating + "%** - Meh :cold_sweat:")
 if(rating < 60) return message.channel.send("**" + message.author.username + "** :heart: **" + user.username + "** **|** **" + rating + "%** - Great :smile:")
 if(rating > 80) return message.channel.send("**" + message.author.username + "** :heart: **" + user.username + "** **|** **" + rating + "%** - Amazing :heart_eyes:")
-if(rating === 100) return message.channel.send("**" + message.author.username + "** :heart: **" + user.username + "** **|** **" + rating + "%** - PERFECT :heart_exclamation:")	
+if(rating > 100) return message.channel.send("**" + message.author.username + "** :heart: **" + user.username + "** **|** **100%** - PERFECT :heart_exclamation:")	
 };
 
 exports.conf = {
